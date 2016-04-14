370537c1420132e42e4931527c87da2a66893482
akguptakgp <ankitkumargupta1996@gmail.com>
2016-04-14 09:50:05 +0530

    communication working

--- src/client.py
+++ src/client.py
@@ -1,7 +1,9 @@
-import socket
+import socket,sys
+from threading import Thread
+import random
+from datetime import datetime
+import sys
 
-HOST = '10.5.16.194'    # chat server IP
-PORT= 50007              # port as used by the server
 class VectorClock(object):
 	def __init__(self):
 		self.Max_Clients=100
@@ -15,44 +17,178 @@ class VectorClock(object):
 		b=b.split(',')
 		for i in range(self.Max_Clients):
 			self.vector_clock[i]=max(self.vector_clock[i],int(b[i]))
+
 class message(object):
-	def __init__(self,time_stamp,msg,Client_id,Group_id, mid=1):
-		self.message_id=mid
-		self.clock=time_stamp
-		self.text=msg 
-		self.Client_id=Client_id
-		self.Group_id=Group_id
+	def __init__(self,msg,msg_type,Client_id,Group_id,time_stamp=None):
+		self.Client_id=Client_id         # a string value
+		self.Group_id=Group_id			 # a string value
+		self.clock=time_stamp			 # a VectorClock value
+		self.text=msg                    # string
+		self.msg_type=msg_type           # 0 or 1
+	
 	def __str__(self):
-		return str(self.Client_id)+"#"+str(self.text)+"#"+str(self.Group_id)+"#"+str(self.clock)
-		
-class Client(object):
+		if(self.msg_type==0):
+			return "@#"+str(self.Client_id)+"#"+str(self.Group_id)+"#"+str(self.text)+"#"+str(self.clock) # client-client type
+		else:
+			return "^#"+str(self.Client_id)+"#"+str(self.Group_id)+"#"+str(self.text)
+
+class Client(object): # for logout, login for the time assume no logout because we need to store on disk 
 	def __init__(self):
-		self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		print "Please Enter your user ID"
 		self.uid=raw_input()
-		self.execute()
+		self.clocks={}	
+		self.serverIp='localhost'
+		self.serverPort=50089+int(sys.argv[1])
+		self.clientPort=random.randint(10000,60000)
+		self.ClientId_IP={}
+		self.Grp_Info={}
+		self.BufferedMsg={}
+		self.ChattingTable={}
+		self.delay_queue=[]	
+		self.clientIp='localhost'
+		# Create two threads as follows
+		Thread(target=self.execute, args=()).start()
+		Thread(target=self.RecvAndServe, args=()).start()	
 
 	def execute(self):
+		print "thread 1"
 		while(1):
 			print "select option 1,2,3[create,join,chat]"
 			opt=int(raw_input())
 			if(opt==1):
 				print "Enter Group Id you wants to create"
 				gid=raw_input()
-				self.socket.connect((HOST, PORT))
-				self.socket.sendall('join#'+gid+"#"+uid)
+				self.Send_message('join',gid)
 			elif(opt==2):
 				print "Enter Group Id you wants to join"
 				gid=raw_input()
-				self.socket.connect((HOST, PORT))
-				self.socket.sendall('join#'+gid+"#"+uid)
+				self.Send_message('join',gid)
 			elif(opt==3):
 				print "Enter Group Id you wants to chat"
-				gid=raw_input()	
+				gid=raw_input()
+				print "Enter message"
+				msg=raw_input()
+				for i in self.Grp_Info[gid]:
+					self.clocks[gid].increment(int(self.uid)-1) # assume node id to be self.uid-1
+					if(self.Grp_Info[gid][i]!=self.uid):
+						self.Send_message(msg,gid,self.ClientId_IP[i][0],int(self.ClientId_IP[i][1]),Client=True)
+			elif(opt==5):
+				print "Enter Group Id you wants to leave"
+				gid=raw_input()
+				self.Send_message('leave',gid)
+			elif(opt==6): # logout
+				print "Enter Group Id you wants to join"
+				gid=raw_input()
+				self.Send_message('join',gid)	
+			elif(opt==4):
+				sys.exit(0)
 	
-	def Send_message(self,m):
-		pass
+	def Send_message(self,msg_text,gid,host=None,port=None,Client=False):
+		soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		if(Client):
+			soc.connect((host, port))
+			soc.sendall(str(message(msg_text,0,self.uid,gid,self.clocks[gid])))
+		else:
+			soc.connect((self.serverIp,self.serverPort))
+			soc.sendall(str(message(msg_text+'/'+self.clientIp+'/'+str(self.clientPort),1,self.uid,gid)))
+			# soc.sendall("^#"+str(self.uid)+'#'+str(gid)+'#'+msg_text+'/'+self.clientIp+'/'+str(self.clientPort))
+		soc.close()	
+	def handle_server(self,msg): # ignore uid send by server
+		parsed=msg.text.split('/')
+		if(parsed[0]=="InformJoin"):
+			print "join"
+			self.ClientId_IP[parsed[1]]=(parsed[2],int(parsed[3]))
+			if(msg.Group_id in self.Grp_Info):
+				self.Grp_Info[msg.Group_id][parsed[1]]=True
+				print "other joined"
+			else:
+				print "Error"
+		elif(parsed[0]=="InformLeave"):
+			del self.Grp_Info[msg.Group_id][parsed[1]]
+			if(parsed[1]==self.uid):
+				del self.Grp_Info[msg.Group_id]
+			else:
+				for i in self.Grp_Info[msg.Group_id]:
+					print i 
+		elif(parsed[0]=="InformLogout"):
+			self.Grp_Info[msg.Group_id][parsed[1]]=False
+		elif(parsed[0]=="InformLogin"):
+			self.ClientId_IP[msg.Group_id][parsed[1]]=(parsed[2],int(parsed[3]))
+			self.Grp_Info[msg.Group_id][msg.Client_id]=True
+		elif(parsed[0]=="InformJoinsuccess"):
+			self.clocks[msg.Group_id]=VectorClock()
+			self.Grp_Info[msg.Group_id]={}
+			ip_id_list=parsed[1].split(':')
+			for i in ip_id_list:
+				self.ClientId_IP[i.split(',')[0]]=(i.split(',')[1],int(i.split(',')[2]))
+				self.Grp_Info[msg.Group_id][i.split(',')[0]]=True
+				
+
+	def handle_client(self,msg):
+		print msg.text,' received from ',msg.Client_id,msg.Group_id
+
+	def handle(self,string):
+		msg=self.ReadFromString(string)
+		# print msg.msg_type	 
+		if(msg.msg_type==1):
+			self.handle_server(msg)
+		if(msg.msg_type==0):
+			self.handle_client(msg)
+
+	def RecvAndServe(self):
+		soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		soc.bind((self.clientIp,self.clientPort))
+		soc.listen(10)
+		print "client listing at ",str(self.clientPort)
+		while True:
+			conn, addr = soc.accept()
+			print 'Connected with ' + addr[0] + ':' + str(addr[1])
+			self.handle(conn.recv(1024))
+
+	def ReadFromString(self,message_string):
+		msg=message_string.split('#')
+		print msg
+		if(msg[0]=="@"): # client-client type message
+			return message(msg[3],0,msg[1],msg[2],msg[4])
+		elif(msg[0]=="^"): # server-client type message
+			return message(msg[3],1,msg[1],msg[2]) 	
+	
+	# assuming the active members in both the timestamps are same
+	# vectorClock : list
+	def timestamp_compare(msg1,msg2):
+		vectorClock1 = msg1.clock
+		vectorClock2 = msg2.clock
+		ans = True # assuming vectorClock1 > vectorClock2
+		for i in vectorClock1.Max_Clients:
+			if(vectorClock1[i] < vectorClock2[i]):
+				ans = False
+				break
+		return ans
+
+
+	# msg : message type
+	def cbcast(self,msg):
+		delay_delivery = 0
+		clock = self.clocks[msg.Group_id]
+		if(clock.vector_clock[msg.Client_id] != msg.clock.vector_clock[msg.Client_id] - 1):
+			delay_delivery = 1
+		else:
+			for member in self.Grp_Info[msg.Group_id]:
+				if(clock.vector_clock[member] < msg.clock.vector_clock[member]):
+					delay_delivery = 1
+					break
+		if delay_delivery == 1:
+			delay_queue.append(msg)
+			delay_queue.sort(timestamp_compare)
+		else:
+			# deliver(msg)
+			chat.append(msg.Client_id + ' says ' + msg.text)
+			clock.vector_clock[self.uid] += 1
+			for member in self.Grp_Info[msg.Group_id]:
+				clock.vector_clock[member] = max(clock.vector_clock[member],msg.clock.vector_clock[member])		
 
 if __name__=='__main__':
-	# c=Client()
-	print message(VectorClock() ,"hi",5,1)
+	random.seed(datetime.now())
+	c=Client()
+	# print message(VectorClock() ,"hi",5,1)
--- src/server.py
+++ src/server.py
@@ -1,17 +1,93 @@
 import socket
+import sys
 
-HOST = 'localhost'    # chat server IP
-PORT = 50007              # port as used by the server
-Grp_Info={}
-ClientId_IP={}
-#create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST,PORT)) 
-s.listen(5) 
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   print c.recv(1024)
-   c.close()          
+class message(object):
+	def __init__(self,msg,msg_type,Client_id,Group_id,time_stamp=None):
+		self.Client_id=Client_id         # a str value
+		self.Group_id=Group_id		 # a str value
+		self.clock=time_stamp			 # a VectorClock value
+		self.text=msg                    # string
+		self.msg_type=msg_type           # 0 or 1
+	
+	def __str__(self):
+		if(self.msg_type==0):
+			return "@#"+str(self.Client_id)+"#"+str(self.Group_id)+"#"+str(self.text)+"#"+str(self.clock) # client-client type
+		else:
+			return "^#"+str(self.Client_id)+"#"+str(self.Group_id)+"#"+str(self.text)
 
+class Server(object):
+	def __init__(self):
+		self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.serverIp='localhost'
+		self.serverPort=50089+int(sys.argv[1])
+		self.ClientId_IP={}   # int key
+		self.Grp_Info={}      # int key
+		self.execute()
+
+	def execute(self):
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind((self.serverIp,self.serverPort))
+		self.socket.listen(10)
+		print "server listing at ",str(self.serverPort)
+		while True:
+			conn, addr = self.socket.accept()
+			print 'Connected with ' + addr[0] + ':' + str(addr[1])
+			self.handle(conn.recv(1024)) 
+	
+	def handle(self,string):
+		msg=self.ReadFromString(string)
+		parsed=msg.text.split('/')
+		# self.Send_message('fun','fg',self.ClientId_IP[msg.Group_id][0],int(self.ClientId_IP[msg.Group_id][1]))
+		if(parsed[0]=="join"):
+			print "join"
+			self.ClientId_IP[msg.Client_id]=(parsed[1],int(parsed[2]))
+			if(msg.Group_id in self.Grp_Info):
+				self.Grp_Info[msg.Group_id][msg.Client_id]=True
+			else:
+				self.Grp_Info[msg.Group_id]={}
+				self.Grp_Info[msg.Group_id][msg.Client_id]=True
+			for i in self.Grp_Info[msg.Group_id]:
+				if(i!=msg.Client_id and self.Grp_Info[msg.Group_id][i]): # not same client and online
+					self.Send_message('InformJoin'+'/'+str(msg.Client_id)+'/'+parsed[1]+'/'+parsed[2],msg.Group_id,self.ClientId_IP[i][0],int(self.ClientId_IP[i][1]))
+				elif(i==msg.Client_id):
+					id_ip_list=''
+					for j in self.Grp_Info[msg.Group_id]:
+						if(self.Grp_Info[msg.Group_id][i]): # if client online
+							id_ip_list+=':'+str(j)+','+self.ClientId_IP[j][0]+','+str(self.ClientId_IP[j][1])
+					self.Send_message('InformJoinsuccess'+'/'+id_ip_list[1:],msg.Group_id,self.ClientId_IP[msg.Client_id][0],int(self.ClientId_IP[msg.Client_id][1]))
+						
+		elif(parsed[0]=="leave"):
+			for i in self.Grp_Info[msg.Group_id]:
+				if(self.Grp_Info[msg.Group_id][i]):
+					self.Send_message('InformLeave'+'/'+str(msg.Client_id),msg.Group_id,self.ClientId_IP[i][0],int(self.ClientId_IP[i][1]))
+			del self.Grp_Info[msg.Group_id][msg.Client_id]
+			# for i in self.Grp_Info[msg.Group_id]:
+				# print i			
+		elif(parsed[0]=="logout"):
+			self.Grp_Info[msg.Group_id][msg.Client_id]=False
+			for i in self.Grp_Info[msg.Group_id]:
+				if(self.Grp_Info[msg.Group_id][i]!=msg.Client_id):
+					self.Send_message('InformLogout'+'/'+str(msg.Client_id),msg.Group_id,self.ClientId_IP[msg.Group_id][0],int(self.ClientId_IP[msg.Group_id][1]))
+		elif(parsed[0]=="login"):
+			self.Grp_Info[msg.Group_id][msg.Client_id]=True
+			for i in self.Grp_Info[msg.Group_id]:
+				if(self.Grp_Info[msg.Group_id][i]!=msg.Client_id):
+					self.Send_message('InformLogin'+'/'+str(msg.Client_id)+'/'+parsed[1]+'/'+parsed[2],msg.Group_id,self.ClientId_IP[msg.Group_id][0],int(self.ClientId_IP[msg.Group_id][1]))
+	
+	def Send_message(self,msg_text,gid,host=None,port=None,Client=False):
+		soc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		soc.connect((host,int(port)))
+		soc.sendall(str(message(msg_text,1,'ignre',gid)))
+		# soc.sendall("^#"+str("ignre")+'#'+str(gid)+'#'+msg_text)
+		soc.close()	
+
+	def ReadFromString(self,message_string):
+		msg=message_string.split('#')
+		print msg
+		if(msg[0]=="@"): # client-client type message
+			return message(msg[3],0,msg[1],msg[2],msg[4])
+		elif(msg[0]=="^"): # server-client type message
+			return message(msg[3],1,msg[1],msg[2])
+
+if __name__=='__main__':
+	c=Server()
